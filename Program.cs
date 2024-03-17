4e47eee3d604c7a01954c6cc8ff5e9edec1ad39e
mertcansucu <mrtcnscc@gmail.com>
2024-03-18 02:12:41 +0300

    Authentication

--- Program.cs
+++ Program.cs
@@ -22,6 +22,19 @@ builder.Services.Configure<IdentityOptions>(options => {
 
     options.User.AllowedUserNameCharacters = "abcdefghiıjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
+    //kullanıcı login işleminde ben kullanıcıya 5 hak vericem eğer 5 seferde login olamazsa hesabını 5 dk kitliyicem ve kullanıcı giriş yapamıyacak
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options =>{//cookie ayarları
+    options.LoginPath = "/Account/Login";//default halide bu login girişi için buraya yönlendirir beni
+    options.AccessDeniedPath = "/Account/AccessDenied";//giriş yapan kullanıcı rolü yani yetkisi yetersizse sayfalara erişmesini engelliyorum ve kullanıcıya yetkisiz kişi olduğunu söylüyorum,usercontrollera eklediğim kod ile yetkisiz biri yetkisi dışında bir yere gitmek istediğinde bu sayfaya yönlendirilecek
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);//ben bu yazdığım iki kodla diyorum ki kullanıcı 30 gün içerisinde giriş yapsın veya yapmasın cookisini sakla sonra sil, ama diyelim 15. gün ben giriş yaptım süre 30 gün olarak yeniden başlar sonra kayıt silinir ve kullanıcı yeniden giriş yaparak giriş yapmalıdır
+    
+
 });
 
 var app = builder.Build();
@@ -38,6 +51,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+//kimlik doğrulama işlemleri
+app.UseAuthentication();
 
 app.UseAuthorization();
 

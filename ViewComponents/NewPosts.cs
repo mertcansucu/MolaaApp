33232653c423cb0fb0d7ebe1186f21bd0ab71746
mertcansucu <mrtcnscc@gmail.com>
2024-03-26 04:48:15 +0300

    View Components update

--- ViewComponents/NewPosts.cs
+++ ViewComponents/NewPosts.cs
@@ -5,22 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MolaaApp.Data.Abstract;
+using MolaaApp.Models;
 
 namespace MolaaApp.ViewComponents
 {
     public class NewPosts : ViewComponent
     {
         private IPostRepository _postRepository;
-        public NewPosts(IPostRepository postRepository){
+        public NewPosts(IPostRepository postRepository)
+        {
             _postRepository = postRepository;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(){
-            return View( await _postRepository
-                                .Posts
-                                .OrderByDescending(p => p.PubilshedOn)//veri tabanındaki bilgileri azalan şekilde sıralattım
-                                .Take(5)//son 5 veriyi aldım
-                                .ToListAsync());
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _postRepository.Posts
+                .Where(p => p.IsActive) // Sadece etkin gönderileri filtrele
+                .OrderByDescending(p => p.PubilshedOn)
+                .Take(5)
+                .ToListAsync();
+
+            return View(posts);
         }
     }
 }
